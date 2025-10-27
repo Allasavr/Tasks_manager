4eca0ac1652b2eb343f25c012ab47d454301e601
Allasavr <allasavr2005@gmail.com>
2025-10-27 10:29:39 +0400

    feat: LAB4-3 Добавлены ранее созданные страницы в проект

--- webpack.config.js
+++ webpack.config.js
@@ -13,21 +13,55 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [
+         { 
+           loader:'file-loader',
+           options:{
+             name: '[name].[ext]',
+             outputPath:'img/'
+           }
+         }
+        ],
+      },
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      inject: true,
-      chunks: ['index'],
-      filename: 'index.html',
-    }),
+  new HtmlWebpackPlugin({
+    template: './src/main.html',
+    filename: 'main.html',
+  }),
+  new HtmlWebpackPlugin({
+    template: './src/about.html',
+    filename: 'about.html',
+  }),
+  new HtmlWebpackPlugin({
+    template: './src/projects.html',
+    filename: 'projects.html',
+  }),
+  new HtmlWebpackPlugin({
+    template: './src/tasks.html',
+    filename: 'tasks.html',
+  }),
+  new HtmlWebpackPlugin({
+    template: './src/img/404 error with people holding the numbers-bro.svg',
+    filename: 'src/img/404 error with people holding the numbers-bro.svg',
+  }),
+  new HtmlWebpackPlugin({
+    template: './src/img/free-icon-list-5709520.png',
+    filename: 'src/img/free-icon-list-5709520.png',
+  }),
+  new HtmlWebpackPlugin({
+    template: './src/img/Accept tasks-bro.svg',
+    filename: 'src/img/Accept tasks-bro.svg',
+  }),
   ],
   devServer: {
     static: {
       directory: path.join(__dirname, 'dist'),
     },
-    open: true,
+    open: 'main.html',
   },
   mode: 'development',
 };
